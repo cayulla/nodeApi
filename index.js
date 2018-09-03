b09f26e1f444fbe99520a9464e434ee174f72300
Julio Cayulla <jcayulla@gmail.com>
2018-09-02 21:19:42 -0500

    cambios

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').__express);
 
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/',(req,res,next)=>{
